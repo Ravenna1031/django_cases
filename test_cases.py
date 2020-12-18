b56e0e99d416e5432e76e83529349aa6e6671031
unknown <Ravenna1031>
2020-12-18 14:21:02 +0800

    add

--- test_cases.py
+++ test_cases.py
@@ -1,7 +1,8 @@
 from API import *
 import pytest
+import allure
 
-
+@allure.feature("test start")
 class TestAPI:
     def test_nums(self, get_nums_data):
         response = nums(get_nums_data[0], get_nums_data[1])
